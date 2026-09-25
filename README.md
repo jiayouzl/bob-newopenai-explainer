1afd47e6d452cad864a4cd512b6b24df89896252
ZhangLei <jiayouzl@vip.qq.com>
2026-09-25 12:56:48 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,32 @@
+<p align="center"><img width="100" src="icon.png"></img></p>
+
+基于 [Mopip77](https://github.com/Mopip77/bob-openai-explainer/releases) 二次开发，兼容bob新版markdown渲染。
+
+# bob-openai-explainer
+
+借用 `bob` 的展示能力，使用 `ChatGPT` 快速对所选文本进行解释说明
+
+## Install
+
+在 [Release](https://github.com/Mopip77/bob-openai-explainer/releases) 页面下载并安装
+
+## Usage
+
+建议 `隐藏并钉到语言切换栏`，按需使用
+
+> <img width="500" src="static/example2.gif"></img>
+
+## Configuration
+
+| key    | defaultValue   |
+| ------ | -------------- |
+| prompt | 简明扼要地解释 |
+
+## Roadmap
+
+- [x] 基础调用
+- [x] 支持插件自动更新
+- [x] 支持流式输出，参考 <https://github.com/openai-translator/bob-plugin-openai-translator/pull/83>
+- [x] 自定义 endpoint
+- [x] 支持自定义模型
+- [x] 支持展示额外调用信息(model、token usage)
---
+++ main.js
@@ -0,0 +1,142 @@
+/**
+ * 由于各大服务商的语言代码都不大一样，
+ * 所以我定义了一份 Bob 专用的语言代码，以便 Bob 主程序和插件之间互传语种。
+ * Bob 语言代码列表 https://bobtranslate.com/plugin/addition/language.html#%E8%AF%AD%E8%A8%80%E4%BB%A3%E7%A0%81
+ *
+ * 转换的代码建议以下面的方式实现，
+ * `xxx` 代表服务商特有的语言代码，请替换为真实的，
+ * 具体支持的语种数量请根据实际情况而定。
+ *
+ * Bob 语言代码转服务商语言代码(以为 'zh-Hans' 为例): var lang = langMap.get('zh-Hans');
+ * 服务商语言代码转 Bob 语言代码: var standardLang = langMapReverse.get('xxx');
+ */
+
+function supportLanguages() {
+    return [
+        "auto",
+        "zh-Hans",
+        "zh-Hant",
+        "yue",
+        "wyw",
+        "pysx",
+        "en",
+        "ja",
+        "ko",
+        "fr",
+        "de",
+        "es",
+        "it",
+        "ru",
+        "pt",
+        "nl",
+        "pl",
+        "ar",
+    ];
+}
+
+function translate(query, completion) {
+    var header = {
+        Authorization: "Bearer " + $option.openaiApiKey,
+        "Content-Type": "application/json",
+    };
+
+    var body = {
+        model: $option.specificModel || $option.model,
+        stream: true,
+        stream_options: {
+            include_usage: true,
+        },
+        messages: [
+            {
+                role: "system",
+                content: `${$option.prompt}\n\n${query.text}`,
+            },
+        ],
+    };
+
+    let targetText = "";
+    let model = "";
+    let usage = {};
+    let streamBuffer = "";
+    function handleStreamData(streamText, query) {
+        $log.info(`[handleStreamData] received streamText => ${streamText}`);
+        if (streamText.startsWith("data: ")) {
+            streamText = streamText.slice(6);
+        }
+
+        if (streamText === "[DONE]") {
+            query.onCompletion({
+                result: {
+                    from: query.detectFrom,
+                    to: query.detectTo,
+                    toParagraphs: [targetText],
+                    toDict:
+                        $option.showExtraInfo === "true"
+                            ? {
+                                  parts: [
+                                      {
+                                          part: "model.",
+                                          means: [model],
+                                      },
+                                      {
+                                          part: "tokens.",
+                                          means: [
+                                              `I:${usage.prompt_tokens}`,
+                                              `O: ${usage.completion_tokens}`,
+                                              `T: ${usage.total_tokens}`,
+                                          ],
+                                      },
+                                  ],
+                              }
+                            : {},
+                },
+            });
+            return;
+        }
+
+        streamBuffer += streamText;
+
+        let dataObj;
+        try {
+            dataObj = JSON.parse(streamBuffer.trim());
+        } catch (err) {
+            $log.info(`[handleStreamData] json not assembled, streamBuffer => ${streamBuffer}`);
+            return;
+        }
+
+        usage = dataObj.usage || usage;
+        model = dataObj.model;
+        if (dataObj.choices.length == 0) {
+            return;
+        }
+        const content = dataObj.choices[0].delta.content;
+        if (content) {
+            targetText += content;
+        }
+
+        query.onStream({
+            result: {
+                from: query.detectFrom,
+                to: query.detectTo,
+                toParagraphs: [targetText],
+            },
+        });
+
+        streamBuffer = "";
+    }
+
+    $http.streamRequest({
+        method: "POST",
+        url: `${$option.endpoint}/chat/completions`,
+        header,
+        body,
+        streamHandler: function (streamData) {
+            streamData.text
+                .split("\n")
+                .filter((line) => line)
+                .forEach((line) => {
+                    handleStreamData(line, query);
+                });
+        },
+    });
+}
